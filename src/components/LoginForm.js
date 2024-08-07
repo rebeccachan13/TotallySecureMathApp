986ceac0047e602426fa9687c2ec3074baad46a3
khrisell <khrisellmarie.porte@edu.sait.ca>
2024-08-07 14:33:09 -0600

    Implemented input validation in forms to enhance security

---
+++ src/components/LoginForm.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { validateInput } from '../utils/inputValidation';
+
+const LoginForm = ({ loginUser }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Validate and sanitize inputs
+    const safeUsername = validateInput(username);
+    const safePassword = validateInput(password);
+
+    // Use validated inputs
+    loginUser(safeUsername, safePassword);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default LoginForm;
